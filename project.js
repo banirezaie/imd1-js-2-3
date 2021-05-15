b4571d1e5b7bd28fbd8d45ae92aa47de7eb8aea2
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-15 18:52:40 +0430

    project done

--- project.js
+++ project.js
@@ -1,22 +1,11 @@
-const button = document.querySelector("button")
-const h1 = document.querySelector("h1")
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
 
-// these are same:
-// document.querySelector('body')
-// document.body
-// rgb(0, 255, ??? )
+const randColor = () => (newColor = `rgb(${randNum()}, ${randNum()}, ${randNum()})`);
+const randNum = () => Math.floor(Math.random() * 256);
 
 button.addEventListener("click", () => {
-  //   console.log("clicked me")
-  // document.body.style.backgroundColor = "olive";
-  const newColor = randomColor()
-  document.body.style.backgroundColor = newColor
-  h1.innerText = newColor
-})
-
-const randomColor = () => {
-  const r = Math.floor(Math.random() * 255)
-  const g = Math.floor(Math.random() * 255)
-  const b = Math.floor(Math.random() * 255)
-  return (newColor = `rgb(${r}, ${g}, ${b})`)
-}
+	const newColor = randColor();
+	document.body.style.backgroundColor = newColor;
+	h1.innerText = newColor;
+});
