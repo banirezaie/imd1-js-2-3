277c71abc0ae9462a3213a0c5dd332e7dc508acf
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-15 18:23:45 +0430

    quiz1 done

--- quiz1.js
+++ quiz1.js
@@ -1,2 +1,5 @@
-//use addEventListener()
-//add a click listener to each button, so when you click on each button console log hello and goodbye
+// use addEventListener()
+// add a click listener to each button, so when you click on each button console log hello and goodbye
+
+document.getElementById("hello").addEventListener("click", () => console.log("hello"));
+document.getElementById("goodbye").addEventListener("click", () => console.log("goodbye"));
